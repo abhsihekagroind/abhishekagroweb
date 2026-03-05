2f78c67c4b55292948ceabb1121360261c0b808b
abhsihekagroind <info.abhishekagroindustry@gmail.com>
2026-03-05 18:44:42 +0545

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+// Firebase configuration - PASTE YOUR CONFIG HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyBvJoIP0-rCSnkwwm6ATqIFemsxgfmla7g",
+    authDomain: "abhishek-agro-admin.firebaseapp.com",
+    projectId: "abhishek-agro-admin",
+    storageBucket: "abhishek-agro-admin.firebasestorage.app",
+    messagingSenderId: "507703167668",
+    appId: "1:507703167668:web:c5e796ee717fb8c46a65e1"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Make available globally
+window.auth = auth;
+window.db = db;
